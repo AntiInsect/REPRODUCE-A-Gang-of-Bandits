84dd10b596107e644b0323250bbc28d3a91f8b2c
brycegbern <56036509+brycegbern@users.noreply.github.com>
2019-10-23 22:02:52 -0500

    Update main.py
    Changed style and fixed for loop

--- main.py
+++ main.py
@@ -15,21 +15,19 @@ def main():
 
     # Instantiating userContextManager and agent
     userContextManager, network = load_data(dataset_location)
-    agent = abstractAgent(algorithm_name)
+    agent = AbstractAgent(algorithm_name)
     
     # The list of results
     results = []
 
     # Main for loop
-    for step in time_steps:
-        user_id, contexts = userContextManager.getUserAndContexts()
+    for step in range(time_steps):
+        user_id, contexts = UserContextManager.get_user_and_contexts()
         chosen_context = agent.choose(user_id, contexts)
-        payoff = userContextManager.getPayoff(user_id, chosen_context)
+        payoff = UserContextManager.get_payoff(user_id, chosen_context)
         agent.update(payoff)
         
         results = results.append(payoff)
-
-        step += 1
 
 if __name__ == '__main__':
     main()
