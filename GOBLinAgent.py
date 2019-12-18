0fce8e276644c77235b35b4f58c7e605d98659be
knospe <willk4898@gmail.com>
2019-12-18 18:52:01 -0500

    More sparse matrices used in GOBLinAgent

--- GOBLinAgent.py
+++ GOBLinAgent.py
@@ -9,9 +9,10 @@ class GOBLinAgent(AbstractAgent):
     def __init__(self, graph, num_users, vector_size=25, alpha=2):
         self.vector_size = vector_size
         self.num_users = num_users
+        # alpha is measure of learning rate
         self.alpha = alpha
         self.bias = numpy.zeros(vector_size * num_users)
-        self.m = numpy.identity(num_users * vector_size)
+        self.m = scp_sp.identity(num_users * vector_size)
         i_n = numpy.identity(num_users)
         laplacian = scp_sp.csgraph.laplacian(graph)
         a = i_n + laplacian
@@ -20,12 +21,13 @@ class GOBLinAgent(AbstractAgent):
         self.a_kron_exp = fractional_matrix_power(self.a_kron, -1/2)
 
     def calculate_score(self, phi, timestep, w_t):
-        ucb = self.alpha * numpy.sqrt(multi_dot([numpy.transpose(phi), numpy.linalg.inv(self.m), phi]) * math.log(timestep + 1))
-        return float(numpy.dot(w_t, phi) + ucb)
+        ucb = self.alpha * numpy.sqrt(multi_dot([numpy.transpose(phi), self.m_inverse_temp.toarray(), phi]) * math.log(timestep + 1))
+        return float(w_t.dot(phi) + ucb)
     
     def choose(self, user_id, contexts, timestep):
-        m_inverse = numpy.linalg.inv(self.m)
-        w_t = numpy.matmul(m_inverse, self.bias)
+        m_inverse = scp_sp.linalg.inv(self.m)
+        self.m_inverse_temp = m_inverse
+        w_t = m_inverse.dot(self.bias)
         new_contexts = []
         for context in contexts:
             context_id, context_vector = context
@@ -45,5 +47,8 @@ class GOBLinAgent(AbstractAgent):
     def update(self, payoff, context, user_id):
         context_id, context_vector = context
         phi = self.context_ids_to_phis[context_id]
-        self.m = self.m + numpy.matmul(phi, numpy.transpose(phi))
-        self.bias = self.bias + phi * payoff
+        phi = numpy.expand_dims(phi, axis=0)
+        dense_to_add = numpy.matmul(numpy.transpose(phi), phi)
+        sparse_to_add = scp_sp.csc_matrix(dense_to_add)
+        self.m = self.m + sparse_to_add 
+        self.bias = self.bias + numpy.squeeze(phi) * payoff
